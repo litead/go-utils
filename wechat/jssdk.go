e333d882899885a642d94c9bac84958aaf4b77f0
zhangbomin <zhangbomin@admixture.com.cn>
2017-08-17 08:56:27 +0800

    wechat: refactor NewJSSDKConfig

--- wechat/jssdk.go
+++ wechat/jssdk.go
@@ -3,29 +3,38 @@ package wechat
 import (
 	"crypto/sha1"
 	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 type JSSDKConfig struct {
-	Debug     bool     `json:"debug"`
-	AppID     string   `json:"appId"`
-	Timestamp uint32   `json:"timestamp"`
-	Nonce     string   `json:"nonceStr"`
-	Signature string   `json:"signature"`
-	ApiList   []string `json:"jsApiList"`
+	Beta      bool   `json:"beta"`
+	Debug     bool   `json:"debug"`
+	AppID     string `json:"appId"`
+	Timestamp uint32 `json:"timestamp"`
+	Nonce     string `json:"nonceStr"`
+	Signature string `json:"signature"`
+	// exclude API list because this need to be configured in
+	// web pages at most of the time
+	//	ApiList   []string `json:"jsApiList"`
 }
 
-func (c *Client) NewJSSDKConfig(timestamp uint32, nonce, url string) *JSSDKConfig {
+func (c *Client) NewJSSDKConfig(beta, debug bool, url string) *JSSDKConfig {
+	cfg := JSSDKConfig{
+		Beta:      beta,
+		Debug:     debug,
+		AppID:     c.AppID,
+		Timestamp: uint32(time.Now().Unix()),
+		Nonce:     strconv.FormatUint(rand.Uint64(), 36),
+	}
+
 	str := fmt.Sprintf("jsapi_ticket=%s&noncestr=%s&timestamp=%v&url=%s",
 		c.JSTicket.Value(),
-		nonce,
-		timestamp,
+		cfg.Nonce,
+		cfg.Timestamp,
 		url)
-	data := sha1.Sum([]byte(str))
+	cfg.Signature = fmt.Sprintf("%02x", sha1.Sum([]byte(str)))
 
-	return &JSSDKConfig{
-		AppID:     c.AppID,
-		Timestamp: timestamp,
-		Nonce:     nonce,
-		Signature: fmt.Sprintf("%02x", data),
-	}
+	return &cfg
 }
