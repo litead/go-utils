e517f04627f29df8a7c665c82385a17a065353fa
zhangbomin <zhangbomin@admixture.com.cn>
2017-10-19 13:59:47 +0800

    taobao: parameter to get all favorites items

--- ali/taobao/client.go
+++ ali/taobao/client.go
@@ -57,17 +57,7 @@ type Client struct {
 	baseURL   string
 }
 
-func NewClient(appKey, appSecret string, env uint) *Client {
-	var baseURL string
-	if env == EnvironmentSandBox {
-		baseURL = "https://gw.api.tbsandbox.com/router/rest"
-	} else if env == EnvironmentProduction {
-		baseURL = "https://eco.taobao.com/router/rest"
-	} else if env == EnvironmentOverseas {
-		baseURL = "https://api.taobao.com/router/rest"
-	} else {
-		return nil
-	}
+func NewClient(baseURL, appKey, appSecret string) *Client {
 	return &Client{appKey: appKey, appSecret: appSecret, baseURL: baseURL}
 }
 
@@ -122,7 +112,7 @@ func (c *Client) sendRequest(args []Argument) ([]byte, error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
-func (c *Client) callAPI(method string, args []Argument, resp interface{}) error {
+func (c *Client) call(method string, args []Argument, resp interface{}) error {
 	args = append(args, Argument{Name: "method", Value: method},
 		Argument{Name: "app_key", Value: c.appKey},
 		Argument{Name: "format", Value: "json"},
@@ -155,103 +145,4 @@ func (c *Client) callAPI(method string, args []Argument, resp interface{}) error
 
 	data = data[bytes.IndexByte(data, ':')+1 : len(data)-1]
 	return json.Unmarshal(data, resp)
-}
-
-type UatmTbkItem struct {
-	ID                uint        `json:"num_iid"`
-	Title             string      `json:"title"`
-	PictURL           string      `json:"pict_url"`
-	SmallImages       StringSlice `json:"small_images"`
-	ReservePrice      float64     `json:"reserve_price,string"`
-	ZKFinalPrice      float64     `json:"zk_final_price,string"`
-	Provcity          string      `json:"provcity"`
-	ItemURL           string      `json:"item_url"`
-	ClickURL          string      `json:"click_url"`
-	Nick              string      `json:"nick"`
-	SellerID          uint        `json:"seller_id"`
-	UserType          uint32      `json:"user_type"`
-	Volume            uint32      `json:"volume"`
-	TkRate            float64     `json:"tk_rate,string"`
-	ZkFinalPriceWAP   float64     `json:"zk_final_price_wap,string"`
-	ShopTitle         string      `json:"shop_title"`
-	EventStartTime    Time        `json:"event_start_time"`
-	EventEndTime      Time        `json:"event_end_time"`
-	Type              uint32      `json:"type"`
-	Status            uint32      `json:"status"`
-	Category          uint        `json:"category"`
-	CouponStartTime   Time        `json:"coupon_start_time"`
-	CouponEndTime     Time        `json:"coupon_end_time"`
-	CouponClickURL    string      `json:"coupon_click_url"`
-	CouponInfo        CouponInfo  `json:"coupon_info"`
-	CouponTotalCount  uint32      `json:"coupon_total_count"`
-	CouponRemainCount uint32      `json:"coupon_remain_count"`
-}
-
-func (c *Client) TBKGetUatmFavoritesItem(args []Argument) ([]UatmTbkItem, error) {
-	var resp struct {
-		Results struct {
-			Items []UatmTbkItem `json:"uatm_tbk_item"`
-		} `json:"results"`
-		TotalResults uint32 `json:"total_results"`
-	}
-
-	args = appendFieldsArgument(args, "num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,seller_id,volume,nick,shop_title,zk_final_price_wap,event_start_time,event_end_time,tk_rate,status,type")
-	if e := c.callAPI("taobao.tbk.uatm.favorites.item.get", args, &resp); e != nil {
-		return nil, e
-	}
-
-	return resp.Results.Items, nil
-}
-
-type NTbkItem struct {
-	ID           uint        `json:"num_iid"`
-	Title        string      `json:"title"`
-	PictURL      string      `json:"pict_url"`
-	SmallImages  StringSlice `json:"small_images"`
-	ReservePrice string      `json:"reserve_price"`
-	ZKFinalPrice string      `json:"zk_final_price"`
-	Provcity     string      `json:"provcity"`
-	ItemURL      string      `json:"item_url"`
-	Nick         string      `json:"nick"`
-	SellerID     uint        `json:"seller_id"`
-	UserType     uint32      `json:"user_type"`
-	Volume       uint32      `json:"volume"`
-}
-
-func (c *Client) TBKGetItem(args []Argument) ([]NTbkItem, error) {
-	var resp struct {
-		Results struct {
-			Items []NTbkItem `json:"n_tbk_item"`
-		} `json:"results"`
-		TotalResults uint32 `json:"total_results"`
-	}
-
-	args = appendFieldsArgument(args, "num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,seller_id,volume,nick")
-	if e := c.callAPI("taobao.tbk.item.get", args, &resp); e != nil {
-		return nil, e
-	}
-
-	return resp.Results.Items, nil
-}
-
-type TbkFavorites struct {
-	Type  uint32 `json:"type"`
-	ID    uint   `json:"favorites_id"`
-	Title string `json:"favorites_title"`
-}
-
-func (c *Client) TBKGetUatmFavorites(args []Argument) ([]TbkFavorites, error) {
-	var resp struct {
-		Results struct {
-			Items []TbkFavorites `json:"tbk_favorites"`
-		} `json:"results"`
-		TotalResults uint32 `json:"total_results"`
-	}
-
-	args = appendFieldsArgument(args, "favorites_title,favorites_id,type")
-	if e := c.callAPI("taobao.tbk.uatm.favorites.get", args, &resp); e != nil {
-		return nil, e
-	}
-
-	return resp.Results.Items, nil
 }
---
+++ ali/taobao/tbk.go
@@ -0,0 +1,131 @@
+package taobao
+
+import (
+	"strconv"
+)
+
+type NTbkItem struct {
+	ID           uint        `json:"num_iid"`
+	Title        string      `json:"title"`
+	PictURL      string      `json:"pict_url"`
+	SmallImages  StringSlice `json:"small_images"`
+	ReservePrice string      `json:"reserve_price"`
+	ZKFinalPrice string      `json:"zk_final_price"`
+	Provcity     string      `json:"provcity"`
+	ItemURL      string      `json:"item_url"`
+	Nick         string      `json:"nick"`
+	SellerID     uint        `json:"seller_id"`
+	UserType     uint32      `json:"user_type"`
+	Volume       uint32      `json:"volume"`
+}
+
+func (c *Client) TbkGetItem(args []Argument) ([]NTbkItem, error) {
+	var resp struct {
+		Results struct {
+			Items []NTbkItem `json:"n_tbk_item"`
+		} `json:"results"`
+		TotalResults uint32 `json:"total_results"`
+	}
+
+	args = appendFieldsArgument(args, "num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,seller_id,volume,nick")
+	if e := c.call("taobao.tbk.item.get", args, &resp); e != nil {
+		return nil, e
+	}
+
+	return resp.Results.Items, nil
+}
+
+type TbkFavorites struct {
+	Type  uint32 `json:"type"`
+	ID    uint   `json:"favorites_id"`
+	Title string `json:"favorites_title"`
+}
+
+func (c *Client) TbkGetUatmFavorites(args []Argument) ([]TbkFavorites, error) {
+	var resp struct {
+		Results struct {
+			Items []TbkFavorites `json:"tbk_favorites"`
+		} `json:"results"`
+		TotalResults uint32 `json:"total_results"`
+	}
+
+	args = appendFieldsArgument(args, "favorites_title,favorites_id,type")
+	if e := c.call("taobao.tbk.uatm.favorites.get", args, &resp); e != nil {
+		return nil, e
+	}
+
+	return resp.Results.Items, nil
+}
+
+type UatmTbkItem struct {
+	ID                uint        `json:"num_iid"`
+	Title             string      `json:"title"`
+	PictURL           string      `json:"pict_url"`
+	SmallImages       StringSlice `json:"small_images"`
+	ReservePrice      float64     `json:"reserve_price,string"`
+	ZKFinalPrice      float64     `json:"zk_final_price,string"`
+	Provcity          string      `json:"provcity"`
+	ItemURL           string      `json:"item_url"`
+	ClickURL          string      `json:"click_url"`
+	Nick              string      `json:"nick"`
+	SellerID          uint        `json:"seller_id"`
+	UserType          uint32      `json:"user_type"`
+	Volume            uint32      `json:"volume"`
+	TkRate            float64     `json:"tk_rate,string"`
+	ZkFinalPriceWAP   float64     `json:"zk_final_price_wap,string"`
+	ShopTitle         string      `json:"shop_title"`
+	EventStartTime    Time        `json:"event_start_time"`
+	EventEndTime      Time        `json:"event_end_time"`
+	Type              uint32      `json:"type"`
+	Status            uint32      `json:"status"`
+	Category          uint        `json:"category"`
+	CouponStartTime   Time        `json:"coupon_start_time"`
+	CouponEndTime     Time        `json:"coupon_end_time"`
+	CouponClickURL    string      `json:"coupon_click_url"`
+	CouponInfo        CouponInfo  `json:"coupon_info"`
+	CouponTotalCount  uint32      `json:"coupon_total_count"`
+	CouponRemainCount uint32      `json:"coupon_remain_count"`
+}
+
+func (c *Client) TbkGetUatmFavoritesItem(args []Argument, count int) ([]UatmTbkItem, error) {
+	args = appendFieldsArgument(args, "num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,seller_id,volume,nick,shop_title,zk_final_price_wap,event_start_time,event_end_time,tk_rate,status,type")
+
+	if count <= 0 {
+		count = 0x7fffffff
+	}
+
+	pageSize := count
+
+	if count >= 100 {
+		pageSize = 100
+	}
+
+	args = append(args, Argument{Name: "page_size", Value: strconv.Itoa(pageSize)})
+
+	result := make([]UatmTbkItem, 0, 1024)
+
+	for page := 1; len(result) < count; page++ {
+		var (
+			targs []Argument
+			resp  struct {
+				Results struct {
+					Items []UatmTbkItem `json:"uatm_tbk_item"`
+				} `json:"results"`
+				TotalResults int `json:"total_results"`
+			}
+		)
+
+		targs = append(args, Argument{Name: "page_no", Value: strconv.Itoa(page)})
+
+		if e := c.call("taobao.tbk.uatm.favorites.item.get", targs, &resp); e != nil {
+			return nil, e
+		}
+
+		result = append(result, resp.Results.Items...)
+		if pageSize > len(resp.Results.Items) {
+			break
+		}
+	}
+
+	return result, nil
+}
